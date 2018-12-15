8b603816ac9ef0cf095a160a064c4c4f7ac50127
Rohit Patil <rohts.patil@gmail.com>
2018-12-16 00:01:39 +0530

    spring xml

--- src/main/java/com/pluralsight/app/Application.java
+++ src/main/java/com/pluralsight/app/Application.java
@@ -1,12 +1,15 @@
 package com.pluralsight.app;
 
 import com.pluralsight.service.CustomerService;
-import com.pluralsight.service.CustomerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
 
   public static void main(String[] args) {
-    CustomerService service = new CustomerServiceImpl();
+    ApplicationContext applicationContext =
+        new ClassPathXmlApplicationContext("applicationContext.xml");
+    CustomerService service = applicationContext.getBean("customerService", CustomerService.class);
     System.out.print(service.findAll().get(0).getFirstName());
   }
 }
--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -2,16 +2,29 @@ package com.pluralsight.service;
 
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
-import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
 
 import java.util.List;
 
 public class CustomerServiceImpl implements CustomerService {
 
-  private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+  private CustomerRepository customerRepository;
+
+  public CustomerServiceImpl(CustomerRepository customerRepository) {
+    this.customerRepository = customerRepository;
+  }
+
+  public CustomerServiceImpl() {}
 
   @Override
   public List<Customer> findAll() {
     return customerRepository.findAll();
+  }
+
+  public CustomerRepository getCustomerRepository() {
+    return customerRepository;
+  }
+
+  public void setCustomerRepository(CustomerRepository customerRepository) {
+    this.customerRepository = customerRepository;
   }
 }
