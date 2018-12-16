3e0b076afc9819759da46e84ded0d7222c244f10
Rohit Patil <rohts.patil@gmail.com>
2018-12-16 21:29:54 +0530

    use autowired

--- src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -1,10 +1,12 @@
 package com.pluralsight.repository;
 
 import com.pluralsight.model.Customer;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository("customerRepository")
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 
   @Override
--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -2,29 +2,25 @@ package com.pluralsight.service;
 
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
 
+  // @Autowired
   private CustomerRepository customerRepository;
-
-  public CustomerServiceImpl(CustomerRepository customerRepository) {
-    this.customerRepository = customerRepository;
-  }
-
-  public CustomerServiceImpl() {}
 
   @Override
   public List<Customer> findAll() {
     return customerRepository.findAll();
   }
 
-  public CustomerRepository getCustomerRepository() {
-    return customerRepository;
-  }
-
+  @Autowired
   public void setCustomerRepository(CustomerRepository customerRepository) {
-    this.customerRepository = customerRepository;
+    System.out.println("Using setter injection");
+      this.customerRepository = customerRepository;
   }
 }
